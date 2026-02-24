2929411aa64eb9fd09bfbae6ad208e52818e6741
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-02-24 16:41:00 +0000

    general function for checking resources being built

--- coffee_machine.py
+++ coffee_machine.py
@@ -13,20 +13,41 @@ def report() :
     print(f"money: {money}")
     
 
+# def check_resources(order):
+#     for item in menu.MENU :
+#         if order == "espresso":
+#             for ingredient in menu.MENU[es]
+def check_resources(order):
+    for item in menu.MENU :
+        if order == item :
+            print(f"so you want a {item}")
+            for ingredient in menu.MENU[item]["ingredients"]:
+                print(menu.MENU[item]["ingredients"][ingredient])
+               
 
 def use_machine():
     user_input  = input("what would you like? ").lower()
     if user_input == "report":
         report()
-    elif user_input == "latle" :
-        print("make latte")
-    elif user_input == "espresso" :
-        print("make espresso")
-    elif user_input == "capuccino" :
-        print("make capucino")
     elif user_input == "off":
         return
+    else:
+        check_resources(order = user_input)
     use_machine()
+
+# def use_machine():
+#     user_input  = input("what would you like? ").lower()
+#     if user_input == "report":
+#         report()
+#     elif user_input == "latle" :
+#         print("make latte")
+#     elif user_input == "espresso" :
+#         print("make espresso")
+#     elif user_input == "capuccino" :
+#         print("make capucino")
+#     elif user_input == "off":
+#         return
+#     use_machine()
 
 
 use_machine()
