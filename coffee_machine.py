83045b2051c015682582fc43d399f3c505b1d17c
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-02-24 02:44:35 +0000

    report function fully funtional

--- coffee_machine.py
+++ coffee_machine.py
@@ -3,11 +3,30 @@ import menu
 # print(menu.MENU["latte"])
 # print(menu.resources)
 
+money = 0 
+
+def report() :
+    
+    print(f"water: {menu.resources["water"]}ml")
+    print(f"milk: {menu.resources["milk"]}ml")
+    print(f"coffee: {menu.resources["coffee"]}g")
+    print(f"money: {money}")
+    
+
+
 def use_machine():
     user_input  = input("what would you like? ").lower()
     if user_input == "report":
-        print("make a function that prints the report and put it here")
+        report()
+    elif user_input == "latle" :
+        print("make latte")
+    elif user_input == "espresso" :
+        print("make espresso")
+    elif user_input == "capuccino" :
+        print("make capucino")
     elif user_input == "off":
         return
     use_machine()
+
+
 use_machine()
