e15d7b7565f58de972ddc7681e68cec6a21cc2e9
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-02-24 02:23:52 +0000

    use machine bone function created

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,4 +1,13 @@
 import menu 
 
-print(menu.MENU["latte"])
-print(menu.resources)
+# print(menu.MENU["latte"])
+# print(menu.resources)
+
+def use_machine():
+    user_input  = input("what would you like? ").lower()
+    if user_input == "report":
+        print("make a function that prints the report and put it here")
+    elif user_input == "off":
+        return
+    use_machine()
+use_machine()
